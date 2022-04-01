3e6bf9fdbc32abf8dd3967d941834ef9e64eb30a
vinnow98 <evinwoon@gmail.com>
2022-04-01 11:22:47 +0800

    Completed!

--- userAuthentication.go
+++ userAuthentication.go
@@ -1,5 +1,25 @@
 package main
 
+import "fmt"
+
 func main() {
-	//Insert Code here
+	var authenticatedUser = []string{"Robin", "John"}
+	var name string
+	fmt.Println("Please enter your name:")
+	fmt.Scanln(&name)
+	for _, element := range authenticatedUser {
+
+		if element == name {
+			fmt.Println("Welcome!")
+			break
+		} else if name == ("Admin") {
+			fmt.Println("Welcome Admin!")
+			break
+		} else {
+			fmt.Println("Merry men!")
+			break
+		}
+
+	}
+
 }
